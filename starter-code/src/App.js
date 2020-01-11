7609f71b7206501f76b9c895183d4a0321370c98
roberto araya gil <naggas@Naggas.local>
2020-01-11 17:10:16 +0100

    6 step in progress

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,6 +4,7 @@ import Greetings from "./components/Greetings";
 import BoxColor from "./components/BoxColor";
 import Random from "./components/Random";
 import CreditCard from "./components/CreditCard";
+import Rating from "./components/Rating";
 
 class App extends Component {
   render() {
@@ -74,6 +75,16 @@ class App extends Component {
             bgColor="#ddbb55"
             color="white"
           />
+        </div>
+
+        <h1>Rating</h1>
+        <div className="ratingContainer">
+          <Rating>0</Rating>
+          <Rating>1.49</Rating>
+          <Rating>1.5</Rating>
+          <Rating>3</Rating>
+          <Rating>4</Rating>
+          <Rating>5</Rating>
         </div>
       </div>
     );
---
+++ starter-code/src/components/Rating.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Rating.css'
+
+const Rating = () => {
+    return ( 
+        <p><i className="fa fa-star" aria-hidden="true"></i>
+        <i className="fa fa-star" aria-hidden="true"></i>
+        <i className="fa fa-star" aria-hidden="true"></i>
+        <i className="fa fa-star" aria-hidden="true"></i>
+        <i className="fa fa-star-o" aria-hidden="true"></i> </p>
+     );
+}
+ 
+export default Rating;
